611a8e746e82933903cbbf37d2df8a01721c2101
Shawn M. Crowley <scrowley@buffalo.edu>
2025-03-19 10:48:03 -0400

    Adjusted Nextjs Config to Disable PWA in Development

--- next.config.mjs
+++ next.config.mjs
@@ -5,12 +5,12 @@ import withPWA from 'next-pwa';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,      // Enable React strict mode for improved error handling
-    //swcMinify: true,            // Enable SWC minification for improved performance
+    //swcMinify: false,            // This configuration OPTION has been removed and is enabled by DEFAULT in Nextjs 15+
     };
 
 export default withPWA({
     dest: "public",         // destination directory for the PWA files
-   // disable: process.env.NODE_ENV === "development",        // disable PWA in the development environment
+    disable: process.env.NODE_ENV === "development",        // disable PWA in the development environment
     register: true,         // register the PWA service worker
     skipWaiting: true,      // skip waiting for service worker activation
 })(nextConfig);
