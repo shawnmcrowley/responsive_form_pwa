09b3906641ecd7b4ce9a884af3b1712c8ef80847
Shawn M. Crowley <scrowley@buffalo.edu>
2025-04-04 17:40:44 -0400

    Updated Footer and Data Display

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
 
         <footer className="bg-white rounded-lg shadow-sm m-4 dark:bg-gray-800">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="/" className="hover:underline">The Lycra Company™</a>. All Rights Reserved.
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="/" className="hover:underline">The Lycra Company™</a> All Rights Reserved.
                 </span>
                 <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                     <li>
@@ -14,7 +14,7 @@ export default function Footer() {
                         <a href="/api-docs" className="hover:underline me-4 md:me-6">API Documentation</a>
                     </li>
                     <li>
-                        <a href="/formutil" className="hover:underline me-4 md:me-6">Demo</a>
+                        <a href="/ui" className="hover:underline me-4 md:me-6">View Data</a>
                     </li>
                     <li>
                         <a href="mailto:shawn.crowley@lycra.com" className="hover:underline">Contact</a>
