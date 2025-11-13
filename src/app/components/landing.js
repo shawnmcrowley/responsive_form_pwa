19cb8a788c7c1a81b5ba5bf9f450784b082a5e8e
Shawn M. Crowley <scrowley@buffalo.edu>
2025-11-13 12:00:27 -0500

    Updated the Landing Page for AI

--- src/app/components/landing.js
+++ src/app/components/landing.js
@@ -33,6 +33,16 @@ export default function Landing() {
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                     </svg></a>
                 </li>
+                  <li className="mb-10 ms-4">
+                    <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">AI Prompts, Agents, RAG, and Workflows</time>
+                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Zero retention, IP Protection, Validate and Verify Results, Local Models</h3>
+                    <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                        AI usage rule of thumb - Verify and Validate. Market Documents, Intellectural Property, Contracts or Proprietary Content should never be exposed to Public LLM's. AI applications that are developed should be tested and deployed on NON-PUBLIC facing infrastructure or deployed on Local LLM technology.</p>
+                    <a href="https://medium.com/@chadhamoksh/build-a-fully-local-rag-app-with-postgresql-llama-3-2-and-ollama-b18cec13382d" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Learn more <svg className="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                    </svg></a>
+                </li>
                 <li className="mb-10 ms-4">
                     <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
                     <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">DevOps and Agile Development</time>
